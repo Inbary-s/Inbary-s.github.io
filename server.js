66daff19912be48933a3eae4dbd13d3f7adb9926
inbary-s <inbarsim6@gmail.com>
2020-03-31 13:54:09 -0600

    updated server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const path = require('path')
 // Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -8,6 +9,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+app.get('*', (req,res)=>{
+  res.sendFile(path.join(__dirname, './client/build/index.html'))
+})
 // Start the API server
 app.listen(PORT, () =>
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
