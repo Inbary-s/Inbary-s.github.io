3028f36a34cc52ef3c5df7a0c967941d8c62ded3
inbary-s <inbarsim6@gmail.com>
2020-04-04 17:21:32 -0600

    about page

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -2,12 +2,10 @@ import React, { useState } from 'react';
 
 const Project = (props) => {
     const [projectCard, setProjectCard] = useState({
-        src: props.data.src,
-        tooltip: true
+        src: props.data.src
     })
 
     const handleHover = (e) => {
-        // console.log (props);
         setProjectCard({ src: props.data.dataHover })
     }
 
@@ -15,11 +13,21 @@ const Project = (props) => {
         setProjectCard({ src: props.data.src })
     }
 
+    // console.log(props.data.dataTooltip)
     // href={props.data.href}
     return(
-        <div className="projects col s6">
-            <img onMouseOver={handleHover} onMouseOut={handleAway} className="project" src={projectCard.src} data-hover={props.data.dataHover} className="images" alt={props.data.alt}/>
-            <div className="tooltipped" data-position={props.data.dataPosition} data-tooltip={props.data.dataTooltip}></div>
+        <div className="projects col s5 card sticky-action">
+            <div className="card-image card-action waves-effect waves-block waves-light">
+                <img onMouseOver={handleHover} onMouseOut={handleAway} className="project activator" src={projectCard.src} data-hover={props.data.dataHover} className="images" alt={props.data.alt}/>
+            </div>
+            <div className="card-content">
+                <span className="card-title activator text-darken-4">{props.data.alt}<i className="material-icons right">more_vert</i></span>
+                <p><a href={props.data.href}>GitHub</a> | <a href={props.data.deployedHref}>Deployed App</a></p>
+                </div>
+            <div className="card-reveal">
+                <span className="card-title grey-text text-darken-4">{props.data.alt}<i className="material-icons right">close</i></span>
+                <p>{props.data.dataTooltip}</p>
+            </div>
         </div>
     )
 }
